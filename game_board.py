559f89d34d906e74b0c965c72ace9375b33f83b6
Andrew McKenzie <124andrew@live.com>
2024-09-01 19:36:13 +1000

    Refactored game_board

--- game_board.py
+++ game_board.py
@@ -12,80 +12,30 @@ class GameBoard():
     def __init__(self):
         # Board is a dictionary of tuples (x, y) to TableCard objects
         self._board: Dict[Tuple[int, int], Optional[TableCard]] = {(x, y): None for x in range(20) for y in range(20)}
-
-        goal_cards = []
-        gold_idx = random.choice([0,1,2])
-
-        for i in range(3):
-            if gold_idx == i:
-                goal_cards.append(GoalCard())
-            else:
-                goal_cards.append(GoalCard())
-
-        self._flippedCards = []
         self._board[(6, 10)] = StartCard()
         self.start_point = (6, 10)
-        self.goal_locations = [(14,8), (14,10), (14,12)]
-        self.gold_loc = self.goal_locations[gold_idx]
-        for i, goal in enumerate(goal_cards):
-            self._board[self.goal_locations[i]] = goal
-
-    def is_connected_start(self, location):
-        seen = set()
-        return self.dfs(location, seen)
-
-    def dfs(self, location, seen):
-        if location == self.start_point:
-            return True
-
-        joins = {
-            dirs.NORTH: dirs.SOUTH,
-            dirs.SOUTH: dirs.NORTH,
-            dirs.EAST: dirs.WEST,
-            dirs.WEST: dirs.EAST,
-        }
-
-        seen.add(location)
-        n, e, s, w = self.get_surrounding(location)
-
-        for next_location in (n, e, s, w):
-            if self.is_within_bounds(next_location) and next_location not in seen and self._board[next_location] is not None:
-                if type(self._board[next_location]) is not DeadEndCard:
-                    board = self._board
-                    next_access = Card.static_access_points(board[next_location].name)
-                    cur_access = Card.static_access_points(board[location].name)
-                    for n in next_access:
-                        if joins[n] in cur_access:
-                            if self.dfs(next_location, seen):
-                                return True
-
-        return False
-
-    def is_within_bounds(self, location):
-        x, y = location
-        return 0 <= x < 20 and 0 <= y < 20
-
-    def get_surrounding(self, location):
-        x, y = location
-        n = (x, y-1)
-        e = (x+1, y)
-        s = (x, y+1)
-        w = (x-1, y)
-        return n, e, s, w
+        self._flippedCards = []  # For storing which board locations have been rotated 180 degrees
+        self.goal_locations = [(14, 8), (14, 10), (14, 12)]
+        gold_idx = random.choice([0, 1, 2])
+        self.gold_loc = self.goal_locations[gold_idx] # Setting gold location for checking later
+        # Setting goal cards
+        for i in range(3):
+            self._board[self.goal_locations[i]] = GoalCard()
 
     def get_gold_location(self):
-        return self.gold_loc
+        return self.gold_loc  # tuple (x, y)
 
     def get_flipped_cards(self):
-        return self._flippedCards
+        return self._flippedCards  # list of tuples (x, y)
 
     def get_board(self):
-        return self._board
+        return self._board  # dictionary of tuples (x, y) to TableCard objects
 
     def copy(self):
         new_board = self._board.copy()
         return new_board
 
+    # Used for adding a new card to the board, enum linked to class instantiation
     def create_card(self, name: Names):
         card_types = {
             Names.CROSS_SECTION: CrossSectionCard(),
@@ -107,12 +57,12 @@ class GameBoard():
         }
         card_class = card_types.get(name.name, None)
         if card_class:
-            #print(card_class)
             return card_class
         else:
-            print("Card not found")
+            print("Card class not found")
             return None
 
+    # Used for flipping a goal card when a path card is placed next to it
     def flip_goal_card(self, loc):
         if loc in self.goal_locations:
             if loc == self.gold_loc:
@@ -120,58 +70,48 @@ class GameBoard():
             else:
                 self._board[loc] = CrossSectionCard()
 
+    # Used for when a player plays map, True if the goal card is the gold card else False
     def peak_goal_card(self, index):
-        if index < 0 or index >= 3:
-            return False
+        # Accepts 0, 1, 2 as index from left to right of the goal cards
+        assert index in [0, 1, 2], "Index must be 0, 1, or 2 for left to right of the goal cards"
         x, y = self.goal_locations[index]
         if (x, y) == self.gold_loc:
             return True
         else:
             return False
 
-    # This method does not check if there is a valid path from
-    # the starting card to the new placed card
+    # Checks if the x, y location we are placing is next to a goal location
+    def check_to_flip_goal_card(self, x, y):
+        surrounds = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+        for loc in surrounds:
+            if loc in self.goal_locations:
+                self.flip_goal_card(loc)
+
+    # Adds a new path card to the board, checking is done in legal actions
     def add_path_card(self, x, y, card_name):
-        #assert isinstance(path_card, PathCard), "The parameter path_card must be an instance of the class PathCard"
-        #assert x >= 0 and x < 20, "The x coordinate must be 0 <= x < 20"
-        #assert y >= 0 and y < 20, "The y coordinate must be 0 <= y < 20"
-        #assert self._board.get_item_value(x, y) is None, "There is already another card on the board at coordinates ({0}, {1})".format(x, y)
-        if not card_name == Names.DYNAMITE:
-            surrounds = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
-            for loc in surrounds:
-                if loc in self.goal_locations:
-                    self.flip_goal_card(loc)
+        assert x >= 0 and x < 20, "The x coordinate must be 0 <= x < 20"
+        assert y >= 0 and y < 20, "The y coordinate must be 0 <= y < 20"
 
+        if not card_name == Names.DYNAMITE:
+            self.check_to_flip_goal_card(x, y)
         self._board[(x, y)] = self.create_card(card_name)
 
+    # For adding a path card in a rotated state (180 degrees)
     def add_flipped_path_card(self, x, y, card_name):
-        #assert isinstance(path_card, PathCard), "The parameter path_card must be an instance of the class PathCard"
-        #assert x >= 0 and x < 20, "The x coordinate must be 0 <= x < 20"
-        #assert y >= 0 and y < 20, "The y coordinate must be 0 <= y < 20"
-        #assert self._board.get_item_value(x, y) is None, "There is already another card on the board at coordinates ({0}, {1})".format(x, y)
+        assert x >= 0 and x < 20, "The x coordinate must be 0 <= x < 20"
+        assert y >= 0 and y < 20, "The y coordinate must be 0 <= y < 20"
 
         if not card_name == Names.DYNAMITE:
-            surrounds = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
-            for loc in surrounds:
-                if loc in self.goal_locations:
-                    self.flip_goal_card(loc)
-
+            self.check_to_flip_goal_card(x, y)
+        # Need to append this location to flipped cards
         self._flippedCards.append((x, y))
-        if (x, y) in self.goal_locations:
-            self.flip_goal_card(x, y)
         self._board[(x, y)] = self.create_card(card_name)
 
+    # For removing a path card using dynamite
     def remove_path_card(self, x, y):
-        #assert x >= 0 and x < 20, "The x coordinate must be 0 <= x < 20"
-        #assert y >= 0 and y < 20, "The y coordinate must be 0 <= y < 20"
-        #assert self._board.get_item_value(x, y) is not None and not self._board.get_item_value(x, y).is_special_card(), "There is no valid card to remove at coordinates ({0}, {1})".format(x, y)
+        assert x >= 0 and x < 20, "The x coordinate must be 0 <= x < 20"
+        assert y >= 0 and y < 20, "The y coordinate must be 0 <= y < 20"
+        # If it's in the flipped cards list, remove it
         if (x, y) in self._flippedCards:
             self._flippedCards.remove((x, y))
-
         self._board[(x, y)] = None
-
-    def get_card_image(self, x, y):
-        card = self._board.get((x, y))
-        if card is not None:
-            return card.image
-        return None
