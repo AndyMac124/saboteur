0269244658c84bd540107ccba5b249a9f4b69753
Andrew McKenzie <124andrew@live.com>
2024-09-01 20:13:19 +1000

    Refactored saboteur_agent

--- saboteur_agent.py
+++ saboteur_agent.py
@@ -7,6 +7,7 @@ class SaboteurAgent(Agent):
     def __init__(self, agent_name, agent_program):
         super().__init__(agent_name, agent_program)
 
+    # Adding all sensors
     def add_all_sensors(self):
         board: Dict[Tuple[int, int], Optional[TableCard]] = {(x, y): None for x in range(20) for y in range(20)}
         self.add_sensor('game-board-sensor', board, lambda m: all(isinstance(opt, TableCard) or isinstance(opt, GoalCard) or opt is None for opt in m.values()))
@@ -18,17 +19,23 @@ class SaboteurAgent(Agent):
         self.add_sensor('deck-status', False, lambda v: isinstance(v, bool))
         self.add_sensor('flipped-cards-sensor', [], lambda v: all(isinstance(c, tuple) for c in v))
         self.add_sensor('known-cards-sensor', [None, None, None], lambda v: isinstance(v, list) and len(v) == 3 and all(isinstance(c, (bool, type(None))) for c in v))
+
+    # Adding all actuators
     def add_all_actuators(self):
         # place, x, y, card
-        # discard, *, *, card
-        # mend, player, *, card
-        # sabotage, player, *, card
-        # map, index, *, card
+        # discard, 0, 0, card
+        # mend, 0, player, card
+        # sabotage, 0, player, card
+        # map, boolean, index, card
+        # dynamite, x, y, card
+        # rotate, x, y, card
+        # pass, *, *, *
         self.add_actuator('play-card', ('place', 0, 0, 0),
                           lambda v: isinstance(v, tuple) and v[0] in ['place', 'discard', 'mend', 'sabotage', 'map', 'dynamite', 'rotate', 'pass'] and v[1] in range(0, 20)
                                     and isinstance(v[1], int) and v[2] in range(0, 20)
                                     and isinstance(v[2], int) and (isinstance(v[3], int)) and v[3] in range(0, 4))
 
+    # Adding all actions
     def add_all_actions(self):
         for i in range(0, 20):
             for j in range(0, 20):
@@ -40,12 +47,6 @@ class SaboteurAgent(Agent):
                     self.add_action('rotate-{0}-{1}-{2}'.format(i, j, k),
                                     lambda x=i, y=j, c=k: {'play-card': ('rotate', x, y, c)})
                     self.add_action('dynamite-{0}-{1}-{2}'.format(i, j, k), lambda x=i, y=j, c=k: {'play-card': ('dynamite', x, y, c)})
-
-        #self.add_action('rotate-true',
-                        #lambda: {'rotate-card': True})
-
-        #self.add_action('rotate-false',
-                       # lambda: {'rotate-card': True})
 
         for i in range(0, 4):
                 self.add_action('discard-{0}-{1}-{2}'.format(0, 0, i),
@@ -59,5 +60,3 @@ class SaboteurAgent(Agent):
             for j in range(0, 3):
                 for k in range(0, 4):
                     self.add_action('map-{0}-{1}-{2}'.format(i, j, k), lambda b=i, x=j, c=k: {'play-card': ('map', b, x, c)})
-
-
